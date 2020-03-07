86fad500943c1bca5aaa53c76a88c5524980116c
sgalati528 <sgalati@iu.edu>
2020-03-07 13:41:00 -0500

    finished project

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -24,10 +24,8 @@ func _physics_process(delta: float) -> void:
 
 func get_direction() -> Vector2:
 	return Vector2(
-		Input.get_action_strength("Move_right") 
-		- Input.get_action_strength("move_left"),
-		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump")
-		else 0.0
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
 	)
 
 
--- src/UserInterface/UserInterface.gd
+++ src/UserInterface/UserInterface.gd
@@ -15,7 +15,7 @@ var paused: = false setget set_paused
 func _ready() -> void:
 	PlayerData.connect("updated", self, "update_interface")
 	PlayerData.connect("died", self, "_on_Player_died")
-	PlayerData.connect("reset", self, "_on_Player_reset")
+	#PlayerData.connect("reset", self, "_on_Player_reset")
 	update_interface()
 
 
@@ -24,8 +24,8 @@ func _on_Player_died() -> void:
 	title_label.text = MESSAGE_DIED
 
 
-func _on_Player_reset() -> void:
-	self.paused = false
+#func _on_Player_reset() -> void:
+	#self.paused = false
 
 
 func _unhandled_input(event: InputEvent) -> void:
